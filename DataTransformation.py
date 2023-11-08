48d7d0c42aa39764963787bf20bb5184d8da3e41
troyfung <fungtroy63@gmail.com>
2023-11-08 22:59:25 +0800

    remove duplicate and convert data to right format

--- DataTransformation.py
+++ DataTransformation.py
@@ -1,4 +1,9 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv
+data = pd.read_csv("hotel_bookings.csv")
+# remove duplicate
+data.drop_duplicates(inplace=True)
+
+# Convert data to right format
+data[''] = pd.to_datetime(data[''])
